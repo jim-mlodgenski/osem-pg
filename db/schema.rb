bf2ec04af1120571a7237748fe33cf1168087267
Jim Mlodgenski <jimm@openscg.com>
2017-04-19 12:11:39 -0400

    Add a flag to simplify merges and clean up the regristration flow

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417152801) do
+ActiveRecord::Schema.define(version: 20170419154936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 20170417152801) do
     t.integer  "end_hour",             default: 20
     t.string   "background_file_name"
     t.boolean  "require_itinerary"
+    t.boolean  "use_pg_flow",        default: true
   end
 
   create_table "conferences_questions", id: false, force: :cascade do |t|
