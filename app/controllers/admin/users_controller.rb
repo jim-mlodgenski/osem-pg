0d9167bd9979ee26435185788a0642c3f848f912
Jim Mlodgenski <jimm@openscg.com>
2017-04-20 13:10:55 -0400

    Added additional required fields for users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -36,7 +36,7 @@ module Admin
       # Variable @show_attributes holds the attributes that are visible for the 'show' action
       # If you want to change the attributes that are shown in the 'show' action of users
       # add/remove the attributes in the following string array
-      @show_attributes = %w(name email username nickname affiliation biography registered attended roles created_at
+      @show_attributes = %w(first_name last_name email username nickname affiliation title mobile biography registered attended roles created_at
                             updated_at sign_in_count current_sign_in_at last_sign_in_at
                             current_sign_in_ip last_sign_in_ip)
     end
@@ -61,8 +61,8 @@ module Admin
     private
 
     def user_params
-      params.require(:user).permit(:email, :name, :email_public, :biography, :nickname, :affiliation, :is_admin,
-                                   :username, :login, :is_disabled, :tshirt, :mobile, :volunteer_experience,
+      params.require(:user).permit(:email, :first_name, :last_name, :email_public, :biography, :nickname, :affiliation, :is_admin,
+                                   :title, :is_admin, :username, :login, :is_disabled, :tshirt, :mobile, :volunteer_experience,
                                    :languages, :to_confirm, :password, role_ids: [])
     end
   end
